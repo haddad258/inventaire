7de219c7567825afe2fd8329f855b22ba5f670a4
haddadrafik258 <haddadrafik258@gmail.com>
2021-01-28 21:18:34 +0100

    init

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
 
 
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const converter = require('json-2-csv');
+fs = require('fs');
+
+const app = express()
+const port = 3000
+app.use(bodyParser.json());
+
+app.post("/getall" , (req,res)=>{
+
+    console.log(req.body.length)
+    console.log(req.body)
+    converter.json2csv(req.body, (err, csv) => {
+      if (err) {
+          throw err;
+      }
+  
+      // print CSV string
+      console.log(csv);
+      fs.writeFileSync('todos.csv', csv);
+  });
+    res.send("ok")
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
